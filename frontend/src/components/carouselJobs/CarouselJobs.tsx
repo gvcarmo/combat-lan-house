0b16f8a967b3d6cb89f9cc4174e0b64aadf9bfed
Giovanni Vittorazze do Carmo <gvittorazze@icloud.com>
2026-02-17 19:17:36 -0300

    .

--- frontend/src/components/carouselJobs/CarouselJobs.tsx
+++ frontend/src/components/carouselJobs/CarouselJobs.tsx
@@ -12,7 +12,13 @@ export const CarouselJobs = () => {
             </div>
             <div className="flex items-center justify-center">
                 <div className="min-[1139px]:w-280 min-[1139px]:h-75 min-[610px]:w-139 min-[610px]:h-[240px] min-[320px]:w-71 min-[320px]:h-[150px] mt-2.5 flex items-center justify-center bg-black">
-                    <img src="./propaganda/promo-25off.gif" alt="" />
+
+                    <picture>
+                        <source srcSet='./propaganda/promo-25off.gif' media='(min-width: 1139px)' />
+                        <source srcSet='./propaganda/promo-25off-tablet.gif' media='(min-width: 610px)' />
+                        <img src='./propaganda/promo-25off-cel.gif' alt='Responsive image' />
+                    </picture>
+
                 </div>
             </div>
         </>
