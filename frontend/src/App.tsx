938ddee2426887f73b919000e9d6803124998635
Giovanni Vittorazze do Carmo <gvittorazze@icloud.com>
2026-02-18 13:50:36 -0300

    .

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,7 +10,6 @@ import { MeusPedidos } from './components/controlPanel/MeusPedidos'
 import { VisualizarPedidoCurriculo } from './components/servicosForm/curriculo'
 import { AcessarPedido } from './components/controlPanelAdmin/AcessarPedido'
 import { EnviarMensagem } from './components/controlPanel/EnviarMensagem'
-import { PedidosPendentes } from './components/controlPanelAdmin/PedidosPendentes'
 
 function App() {
 
