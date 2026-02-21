dea2efd66f69a9a636470a022917bb93104aa21b
Giovanni Vittorazze do Carmo <gvittorazze@icloud.com>
2026-02-21 13:05:59 -0300

    .

--- frontend/src/components/formularioDinamico/FormularioDinamico.tsx
+++ frontend/src/components/formularioDinamico/FormularioDinamico.tsx
@@ -4,7 +4,7 @@ import { useContext } from 'react';
 import { AuthContext } from '../../contexts/AuthContext';
 import { FormIPTU } from '../servicosForm/viaIptu';
 import { FormIPVA } from '../servicosForm/ipva';
-import { CriarVideo } from '../servicosForm/CriarVideo';
+// import { CriarVideo } from '../servicosForm/CriarVideo';
 import { Prazos } from '../../hooks/Prazos';
 
 export const FormularioDinamico = () => {
@@ -31,8 +31,8 @@ export const FormularioDinamico = () => {
                 return <FormIPVA />
             case 'via-licenciamento-anual-do-veiculo':
                 return <FormIPVA />
-            case 'criacao-de-video':
-                return <CriarVideo />
+            // case 'criacao-de-video':
+            //     return <CriarVideo />
             default:
                 return <p>Serviço não encontrado.</p>
         }
